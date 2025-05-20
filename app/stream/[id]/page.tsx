83d887e528a23c6cee543d526b7d4119e99c9fa7
Yak <y@kuhi.to>
2025-05-20 10:31:35 +0300

    this is starting to take shape

--- app/stream/[id]/page.tsx
+++ app/stream/[id]/page.tsx
@@ -1,20 +1,69 @@
 'use client';
 
+import { Address, Clvm, CoinsetClient, Puzzle, StreamedCatParsingResult } from 'chia-wallet-sdk-wasm';
 import { useParams } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+interface StreamData {
+  parsedStreams: StreamedCatParsingResult[] | null;
+}
 
 export default function StreamPage() {
   const params = useParams();
-  const streamId = params.id as string;
+  const streamIdString = params.id as string;
   const [copied, setCopied] = useState(false);
+  const [streamData, setStreamData] = useState<StreamData | null>(null);
 
-  const truncatedId = `${streamId.slice(0, 13)}...${streamId.slice(-6)}`;
+  const truncatedId = `${streamIdString.slice(0, 13)}...${streamIdString.slice(-6)}`;
 
   const handleCopy = async () => {
-    await navigator.clipboard.writeText(streamId);
+    await navigator.clipboard.writeText(streamIdString);
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
   };
+
+  useEffect(() => {
+    if(streamData === null) {
+        (async () => {
+            const streamId = Address.decode(streamIdString).puzzleHash;
+            const client = CoinsetClient.mainnet();
+
+            const eveCoinRecordResponse = await client.getCoinRecordByName(streamId);
+            if (!eveCoinRecordResponse.success ) {
+                alert('Could not fetch eve coin record - stream id may not be valid.');
+                return;
+            }
+        
+            const eveCoinRecord = eveCoinRecordResponse.coinRecord!;
+            const eveCoinSpend = (await client.getPuzzleAndSolution(eveCoinRecord.coin.parentCoinInfo, eveCoinRecord.confirmedBlockIndex)).coinSolution!;
+
+            const ctx = new Clvm();
+
+            const puzz = new Puzzle(eveCoinSpend.coin.puzzleHash, ctx.deserialize(eveCoinSpend.puzzleReveal), eveCoinSpend.coin.puzzleHash);
+            const streamInfo = puzz.parseChildStreamedCat(eveCoinSpend.coin, ctx.deserialize(eveCoinSpend.puzzleReveal), ctx.deserialize(eveCoinSpend.solution))!;
+
+            let parsedStreams: StreamedCatParsingResult[] = [streamInfo];
+
+            let latestStreamResult = streamInfo;
+            while (!latestStreamResult.lastSpendWasClawback) {
+                let coinRecordResp = await client.getCoinRecordByName(latestStreamResult.streamedCat!.coin.coinId());
+                if (!coinRecordResp.success || !coinRecordResp.coinRecord || !coinRecordResp.coinRecord?.spent) {
+                    break;
+                }
+
+                let coinSpend = (await client.getPuzzleAndSolution(coinRecordResp.coinRecord!.coin.coinId(), coinRecordResp.coinRecord!.spentBlockIndex)).coinSolution!;
+                let parsed = puzz.parseChildStreamedCat(coinSpend.coin, ctx.deserialize(coinSpend.puzzleReveal), ctx.deserialize(coinSpend.solution))!;
+                
+                parsedStreams.push(parsed);
+                latestStreamResult = parsed;
+            }
+
+            console.log("Parsed streams #: ", parsedStreams.length);
+            setStreamData({ parsedStreams: parsedStreams });
+
+        })()
+    }
+  }, [streamData]);
 
   return (
     <main className="flex max-w-7xl flex-col m-auto pt-8 px-8">
@@ -27,9 +76,11 @@ export default function StreamPage() {
           {copied ? 'Copied!' : 'Copy ID'}
         </button>
       </div>
-      <div className="text-gray-600">
-        Loading stream details...
-      </div>
+      {
+        streamData === null ? (<div className="text-gray-600">
+            Loading stream details...
+        </div>) : (<div>TODO</div>)
+    } 
     </main>
   );
 } 
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: config => {
+  webpack(config, { isServer, dev }) {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
+
+    // from: https://github.com/vercel/next.js/blob/canary/examples/with-webassembly/next.config.js
+    // Use the client static directory in the server bundle and prod mode
+    // Fixes `Error occurred prerendering page "/"`
+    config.output.webassemblyModuleFilename =
+      isServer && !dev
+        ? "../static/wasm/[modulehash].wasm"
+        : "static/wasm/[modulehash].wasm";
+
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
     return config
-  }
+  },
 }
 
 export default nextConfig;
